b8ee4492d6de50cf76fd0686f40fbfc9e3f308eb
Voronova Ekaterina <e.b.kochekova@gmail.com>
2023-04-30 09:48:13 +0300

    edits based on mentor`s feedback were made

--- conftest.py
+++ conftest.py
@@ -2,10 +2,10 @@ import pytest
 from selene import browser
 
 
-@pytest.fixture
-def init_browser(scope="function"):
-    browser.config.base_url = 'https://demoqa.com/automation-practice-form'
+@pytest.fixture(scope="function", autouse=True)
+def init_browser():
+    browser.config.base_url = 'https://demoqa.com'
     browser.config.window_width = 1920
     browser.config.window_height = 1080
     yield
-    browser.close()
+    browser.quit()
